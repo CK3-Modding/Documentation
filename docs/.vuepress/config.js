dcdf2dcb76ae2d41972469f51e5c380216a888c5
GlorifiedPig <killingpigs123@gmail.com>
2020-09-01 16:52:44 +0200

    appropriate labeling and remove path on /introduction/

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,6 @@ module.exports = {
 		sidebar: [
 			{
 				title: 'Introduction',
-				path: '/introduction/',
 				children: [
 					'/introduction/project-setup',
 					'/introduction/folder-structure'
