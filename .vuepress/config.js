3298d1590e9f901e6d6c5ebc3a9608ced7794b0a
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-09-01 13:26:20 +0100

    add vuepress and plugins locally.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,24 @@
 module.exports = {
+	plugins: {
+		'container': {},
+		'@vuepress/theme-default': {},
+		'@vuepress/active-header-links': {},
+		'@vuepress/nprogress': {},
+		'@vuepress/register-components': {},
+		'@vuepress/search': {}
+	},
+
 	themeConfig: {
 		sidebar: [
+			'/',
+			'/introduction/',
 			{
 				title: 'Introduction',
 				path: '/introduction/',
+				children: [
+					'/',
+					'/project-setup'
+				]
 			}
 		]
 	}
